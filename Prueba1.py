c69328f6802b5044b9b78fcb37be393246ae5f72
UlisesGT1510 <151181125+UlisesGT1510@users.noreply.github.com>
2023-11-22 16:56:38 -0600

    Create Prueba1.py
    Prueba para aplicación

---
+++ Prueba1.py
@@ -0,0 +1,10 @@
+# Este es un comentario en Python
+
+# Imprimir un mensaje en la consola
+print("¡Hola, mundo!")
+
+# Pedir al usuario que ingrese su nombre
+nombre = input("Por favor, ingresa tu nombre: ")
+
+# Imprimir un saludo personalizado
+print("Hola, " + nombre + "! Bienvenido a Python.")
